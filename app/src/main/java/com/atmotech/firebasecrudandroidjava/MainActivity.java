fd865ad1dde001e88b743d7cb7546d6aa5e1def1
wasiul-mannan <amd.wasiulmannan@gmail.com>
2020-10-13 19:46:50 +0600

    Java Started

--- app/src/main/java/com/atmotech/firebasecrudandroidjava/MainActivity.java
+++ app/src/main/java/com/atmotech/firebasecrudandroidjava/MainActivity.java
@@ -1,14 +1,90 @@
 package com.atmotech.firebasecrudandroidjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton fbtn_add;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fbtn_add=findViewById(R.id.fbtn_add);
+        recyclerView=findViewById(R.id.recycler_view);
+
+        fbtn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogAddNote();
+            }
+        });
+    }
+
+    private void showDialogAddNote() {
+        final Dialog dialog=new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_add_note);
+
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(true);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams ();
+        lp.copyFrom(Objects.requireNonNull(dialog.getWindow()).getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(lp);
+
+        ImageButton btnClose = dialog.findViewById(R.id.btn_close);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        final EditText etNote = dialog.findViewById(R.id.etNote);
+        Button btnAdd=dialog.findViewById(R.id.btn_add);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(etNote.getText())) {
+
+                    etNote.setError("This field can't be empty!");
+
+                } else {
+
+                    addDataToFirebase (etNote.getText().toString());
+                }
+            }
+        });
+
+        dialog.show();
+
+    }
+
+    private void addDataToFirebase(String text){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference( "Notes");
     }
 }
